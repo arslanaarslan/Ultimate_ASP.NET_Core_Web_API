66431667c2f08ac35d791d375fbdf3d40c371e40
Arslan Arslan <arslanaarslan@gmail.com>
2024-01-07 08:14:41 +0300

    Add map branch for 'testquerystring' key

--- 1_8_1_Middleware_Example/MiddlewareExample/Program.cs
+++ 1_8_1_Middleware_Example/MiddlewareExample/Program.cs
@@ -34,6 +34,13 @@ app.Map("/usingmapbranch", builder =>
         await context.Response.WriteAsync("Hello from the map branch.");
     });
 });
+app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder =>
+{
+    builder.Run(async context =>
+    {
+        await context.Response.WriteAsync("Hello from the MapWhen branch.");
+    });
+});
 app.Run(async context =>
 {
     Console.WriteLine($"Writing the response to the client in the Run method");
